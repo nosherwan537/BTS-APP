d8e43afd54a93fde95166b7ec73c2854205be425
nosherwan537 <tahirnosherwan@gmail.com>
2024-04-27 01:23:01 +0500

    LATEST COMMIT

---
+++ app/src/main/java/com/example/bts/MapsActivity.java
@@ -0,0 +1,38 @@
+package com.example.bts;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_activity);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Set the map location to Sydney, Australia
+        LatLng sydney = new LatLng(-33.8688, 151.2093);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12));
+
+        // Add a marker in Sydney
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+    }
+}
